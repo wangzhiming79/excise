5136823bf730aa6d29858c26ba65f44398776523
bxc2004025 <billwang@bestaxc.com.cn>
2018-02-12 17:07:08 +0800

    update

--- app/src/main/java/bill/edu/excise/BlankFragment.java
+++ app/src/main/java/bill/edu/excise/BlankFragment.java
@@ -3,6 +3,7 @@ package bill.edu.excise;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
 
 
 /**
@@ -64,26 +91,48 @@ public class BlankFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_blank, container, false);
-
-        v.findViewById(R.id.ivLogo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.medal_anim);
-//                 v.startAnimation(anim);
-                Animator animator = AnimatorInflater.loadAnimator(getContext(),R.animator.scale);
-                animator.setTarget(v);
-                animator.start();
-                //onButtonPressed(Uri.EMPTY);
-            }
-        });
-
-        return  v;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_blank, container, false);
+//
+//
+//        List<PointValue> values = new ArrayList<PointValue>();
+//        values.add(new PointValue(0, 2));
+//        values.add(new PointValue(1, 4));
+//        values.add(new PointValue(2, 3));
+//        values.add(new PointValue(3, 4));
+//
+//        //In most cased you can call data model methods in builder-pattern-like manner.
+//        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+//        List<Line> lines = new ArrayList<Line>();
+//        lines.add(line);
+//
+//        LineChartData data = new LineChartData();
+//        data.setLines(lines);
+//
+//        LineChartView chart = new LineChartView(getContext());
+//        chart.setLineChartData(data);
+//        chart.setInteractive(false);
+//       // chart.setZoomType(ZoomType.HORIZONTAL);
+//        //chart.setContainerScrollEnabled(boolean isEnabled, ContainerScrollType type);
+//        v.addView(chart);
+//
+////        v.findViewById(R.id.ivLogo).setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+//////                Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.medal_anim);
+//////                 v.startAnimation(anim);
+////                Animator animator = AnimatorInflater.loadAnimator(getContext(),R.animator.scale);
+////                animator.setTarget(v);
+////                animator.start();
+////                //onButtonPressed(Uri.EMPTY);
+////            }
+////        });
+//
+//        return  v;
+//    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -122,5 +171,157 @@ public class BlankFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+
+    //=======
+    public final static String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+            "Sep", "Oct", "Nov", "Dec",};
+
+    public final static String[] days = new String[]{"Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun",};
+
+    private LineChartView chartTop;
+    private ColumnChartView chartBottom;
+
+    private LineChartData lineData;
+    private ColumnChartData columnData;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        // *** TOP LINE CHART ***
+        chartTop = (LineChartView) rootView.findViewById(R.id.chart_top);
+
+        // Generate and set data for line chart
+        generateInitialLineData();
+
+        // *** BOTTOM COLUMN CHART ***
+
+        chartBottom = (ColumnChartView) rootView.findViewById(R.id.chart_bottom);
+
+        generateColumnData();
+
+        return rootView;
+    }
+
+    private void generateColumnData() {
+
+        int numSubcolumns = 1;
+        int numColumns = months.length;
+
+        List<AxisValue> axisValues = new ArrayList<AxisValue>();
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        for (int i = 0; i < numColumns; ++i) {
+
+            values = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
+            }
+
+            axisValues.add(new AxisValue(i).setLabel(months[i]));
+
+            columns.add(new Column(values).setHasLabelsOnlyForSelected(true));
+        }
+
+        columnData = new ColumnChartData(columns);
+
+        columnData.setAxisXBottom(new Axis(axisValues).setHasLines(true));
+        columnData.setAxisYLeft(new Axis().setHasLines(true).setMaxLabelChars(2));
+
+        chartBottom.setColumnChartData(columnData);
+
+        // Set value touch listener that will trigger changes for chartTop.
+        chartBottom.setOnValueTouchListener(new ValueTouchListener());
+
+        // Set selection mode to keep selected month column highlighted.
+        chartBottom.setValueSelectionEnabled(true);
+
+        chartBottom.setZoomType(ZoomType.HORIZONTAL);
+
+        // chartBottom.setOnClickListener(new View.OnClickListener() {
+        //
+        // @Override
+        // public void onClick(View v) {
+        // SelectedValue sv = chartBottom.getSelectedValue();
+        // if (!sv.isSet()) {
+        // generateInitialLineData();
+        // }
+        //
+        // }
+        // });
+
+    }
+
+    /**
+     * Generates initial data for line chart. At the begining all Y values are equals 0. That will change when user
+     * will select value on column chart.
+     */
+    private void generateInitialLineData() {
+        int numValues = 7;
+
+        List<AxisValue> axisValues = new ArrayList<AxisValue>();
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (int i = 0; i < numValues; ++i) {
+            values.add(new PointValue(i, 0));
+            axisValues.add(new AxisValue(i).setLabel(days[i]));
+        }
+
+        Line line = new Line(values);
+        line.setColor(ChartUtils.COLOR_GREEN).setCubic(true);
+
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        lineData = new LineChartData(lines);
+        lineData.setAxisXBottom(new Axis(axisValues).setHasLines(true));
+        lineData.setAxisYLeft(new Axis().setHasLines(true).setMaxLabelChars(3));
+      //  lineData.setAxisYRight(new Axis().setHasLines(true).setMaxLabelChars(4));
+
+
+        chartTop.setLineChartData(lineData);
+
+        // For build-up animation you have to disable viewport recalculation.
+        chartTop.setViewportCalculationEnabled(false);
+
+        // And set initial max viewport and current viewport- remember to set viewports after data.
+        Viewport v = new Viewport(0, 1000, 6, 0);
+        chartTop.setMaximumViewport(v);
+        chartTop.setCurrentViewport(v);
+
+        chartTop.setZoomType(ZoomType.HORIZONTAL);
+    }
+
+    private void generateLineData(int color, float range) {
+        // Cancel last animation if not finished.
+        chartTop.cancelDataAnimation();
+
+        // Modify data targets
+        Line line = lineData.getLines().get(0);// For this example there is always only one line.
+        line.setColor(color);
+        for (PointValue value : line.getValues()) {
+            // Change target only for Y value.
+            value.setTarget(value.getX(), (float) Math.random() * range);
+        }
+
+        // Start new data animation with 300ms duration;
+        chartTop.startDataAnimation(300);
+    }
+
+    private class ValueTouchListener implements ColumnChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+            generateLineData(value.getColor(), 100);
+        }
+
+        @Override
+        public void onValueDeselected() {
+
+            generateLineData(ChartUtils.COLOR_GREEN, 0);
+
+        }
     }
 }
